9bcf61934329d0d0e8e0555d01a2d231798cc071
Supriyo Paul <paulsupriyo64nk@gmail.com>
2021-10-18 11:15:31 +0530

    query param

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ class Destination(db.Model):
 
 @app.route('/messages', methods=['GET'])
 def get_message():
-    return {"message": message}
+    return message
 
 
 @app.route('/destinations', methods=['GET'])
